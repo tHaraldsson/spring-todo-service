a2d16e24f1be7cd867f90247644e842bea79cae8
David Karlsson <davidfkarlsson@proton.me>
2025-09-04 15:16:26 +0200

    Project initialized

---
+++ spring-todo-service/src/main/java/com/group5/spring_todo_service/SpringTodoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group5.spring_todo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoServiceApplication.class, args);
+	}
+
+}
