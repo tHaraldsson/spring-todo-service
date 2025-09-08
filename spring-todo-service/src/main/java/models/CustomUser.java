4d434309d9df390dfd400aaec46951fcc8284068
David Karlsson <davidfkarlsson@proton.me>
2025-09-08 11:21:31 +0200

    ADD: class for CustomUser

---
+++ spring-todo-service/src/main/java/models/CustomUser.java
@@ -0,0 +1,49 @@
+package models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "Id")
+    private Long id;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
